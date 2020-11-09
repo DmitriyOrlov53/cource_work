e12087ba429bcc27027dcc8e171f79260cc1aca0
DmitriyOrlov53 <orlov.dmitriy2303@outlook.com>
2020-11-10 00:14:39 +0300

    bug fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ClerkManagementModule } from './clerk-management/clerk-management.module';
 import { StudentsManagementModule } from './students-management/students-management.module';
+import { SubjectsManagementModule } from './subjects-management/subjects-management.module';
 
 @Module({
-	imports: [DatabaseModule, ClerkManagementModule, StudentsManagementModule],
+	imports: [DatabaseModule, ClerkManagementModule, StudentsManagementModule, SubjectsManagementModule],
 })
 export class AppModule {}
--- server/src/students-management/store-of-students/store-of-students.service.ts
+++ server/src/students-management/store-of-students/store-of-students.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CollectionClientService } from 'src/database/collection-client.service';
+import { Document } from 'src/database/objects/documnet.object';
 import { Person } from 'src/database/objects/person.object';
 
 @Injectable()
-export class StoreOfStudentsService extends CollectionClientService<Person> {
+export class StoreOfStudentsService extends CollectionClientService<Person & Document> {
 	readonly collectionName = 'students';
 }
