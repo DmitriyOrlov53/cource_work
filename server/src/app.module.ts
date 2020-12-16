4c11e9ab6d1eb65bfa147cd010e43c7b1977b8bb
DmitriyOrlov53 <orlov.dmitriy2303@outlook.com>
2020-12-16 18:14:16 +0300

    added output of site

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,8 +5,20 @@ import { StudentsManagementModule } from './students-management/students-managem
 import { SubjectsManagementModule } from './subjects-management/subjects-management.module';
 import { GradebookManagementModule } from './gradebook-management/gradebook-management.module';
 import { StatisticModule } from './statistic/statistic.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-	imports: [DatabaseModule, ClerkManagementModule, StudentsManagementModule, SubjectsManagementModule, GradebookManagementModule, StatisticModule],
+	imports: [
+		DatabaseModule,
+		ClerkManagementModule,
+		StudentsManagementModule,
+		SubjectsManagementModule,
+		GradebookManagementModule,
+		StatisticModule,
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '../../../client/dist/client'),
+		}),
+	],
 })
 export class AppModule {}
